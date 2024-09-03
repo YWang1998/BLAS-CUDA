97d9a4dc78f5fdf78cd73797d23fd073a3dfd4af
AnDyW24 <107201655+YWang1998@users.noreply.github.com>
2024-09-03 15:58:07 -0400

    Update cuBLAS.cuh with correct usage of __restrict__ qualifier

--- src/cuBLAS.cuh
+++ src/cuBLAS.cuh
@@ -16,9 +16,9 @@ namespace cuBLAS {
 
 	void spMV_M(dim3& Grid, dim3& Block, const double* d_M, const double* d_V, double* d_Target);
 	void spMV(dim3& Grid, dim3& Block,
-		const double* d_a_expand,
-		const int* d_ja_expand,
-		const double* d_v, double* d_v_expanded, double* d_spMV);
+		const double* __restrict__ d_a_expand,
+		const int* __restrict__ d_ja_expand,
+		const double* __restrict__ d_v, double* __restrict__ d_v_expanded, double* __restrict__ d_spMV);
 	void spMV_thread(dim3& Grid, dim3& Block,
 		const int* d_ia_expand,
 		const int* d_ja_expand,
@@ -31,8 +31,8 @@ namespace cuBLAS {
 		const double* d_a,
 		const double* d_v,
 		double* d_spMV);
-	void dot_product(dim3& Grid, dim3& Block, const double* __restrict__ d_V_1, const double* __restrict__ d_V_2, double* product, double* __restrict__ d_product);
-	void nrm2(dim3& Grid, dim3& Block, const double* __restrict__ V, double* sum, double* __restrict__ d_sum);
+	void dot_product(dim3& Grid, dim3& Block, const double* __restrict__ d_V_1, const double* __restrict__ d_V_2, double* __restrict__ product, double* __restrict__ d_product);
+	void nrm2(dim3& Grid, dim3& Block, const double* __restrict__ V, double* __restrict__ sum, double* __restrict__ d_sum);
 	void axpy(dim3& Grid, dim3& Block, const double* __restrict__ x, double* __restrict__ y, const double& scale);
 	void get_const_int_symbol(const int& h_symbol);
 
